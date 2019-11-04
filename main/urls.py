6ea67fb8e5347b6ec24d1d1f62c0ca19db029a70
cln-m4rie <s.kikkawa0613@gmail.com>
2019-11-05 05:16:39 +0900

    wip

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.top),
+    path('', views.SignupView.as_view()),
 ]
--- main/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-import logging
-from django.http.response import HttpResponse
-
-logger = logging.getLogger(__name__)
-def top(request):
-    logger.info('test')
-    return HttpResponse('hello world')
