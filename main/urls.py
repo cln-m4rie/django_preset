91fecae0b65fc13f23204067ce1270a64c3a3492
cln-m4rie <s.kikkawa0613@gmail.com>
2019-11-11 01:41:05 +0900

    add autopep8

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('signup', views.SignupView.as_view(), name='signup'),
-    path('login', auth_views.LoginView.as_view(template_name='main/registration/login.html'), name='login'),
+    path('login', auth_views.LoginView.as_view(
+        template_name='main/registration/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
